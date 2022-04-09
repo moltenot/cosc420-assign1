e62e17d2a796568e3ab865501b2b34ed58b9a9c8
Theo Molteno <theo@molteno.net>
2022-04-09 15:29:30 +1200

    made run_gan save images

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers
 from utils import IMAGE_SIZE
 
@@ -10,72 +9,72 @@ from utils import IMAGE_SIZE
 # the following models are modified versions of alexnet from this link
 # copied and adjusted from https://towardsdatascience.com/implementing-alexnet-cnn-architecture-using-tensorflow-2-0-and-keras-2113e090ad98
 def make_alexnet_gender_model():
-    return keras.models.Sequential([
-        keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Flatten(),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(2)
+    return tf.keras.models.Sequential([
+        layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Flatten(),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(2)
     ])
 
 def make_alexnet_race_model():
-    return keras.models.Sequential([
-        keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Flatten(),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(5, activation='softmax')
+    return tf.keras.models.Sequential([
+        layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Flatten(),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(5, activation='softmax')
     ])
 
 def make_alexnet_age_model():
-    return keras.models.Sequential([
-        keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-        keras.layers.BatchNormalization(),
-        keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        keras.layers.Flatten(),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(4096, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.Dense(1)
+    return tf.keras.models.Sequential([
+        layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=IMAGE_SIZE + [3]),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        layers.Flatten(),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(4096, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(1)
     ])
 
 
--- run_gan.py
+++ run_gan.py
@@ -1,21 +1,30 @@
+import os
 from matplotlib import pyplot as plt
 import tensorflow as tf
 from models import make_basic_generator_model
 
 
+#################### get the model ####################
 generator = make_basic_generator_model()
 
-checkpoint_dir = './gan-checkpoints/run-2'
+checkpoint_dir = './gan-checkpoints/run-3'
 checkpoint = tf.train.Checkpoint(generator=generator)
-
 
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
-num_examples = 3
-seed = tf.random.normal([num_examples, 100])# 100 is the input dim of the generator
-
+#################### generate images ####################
+num_examples = 10
+seed = tf.random.normal([num_examples, 100]) # 100 is the input dim of the generator
 images = generator(seed)
 
-for image in images:
+#################### save the images ####################
+
+# create the fake subdir if it doesn't exist
+if not os.path.exists('fake'):
+    os.makedirs('fake')
+
+for i, image in enumerate(images):
     plt.imshow(image)
-    plt.show()
+    plt.axis('off')
+    plt.savefig('fake/image_%d.png' % (i+1))
+    # plt.show()
